c13d95ae14017d129ee3324c897f5247ecddb3ca
Nik68rus <Nik200x@mail.ru>
2019-08-19 20:56:43 +0300

    убраз закоментареный код

--- src/util/dom.js
+++ src/util/dom.js
@@ -1,4 +1,2 @@
-//export const getMarkup = (dataList, generator) => dataList.map(generator).join(`\n`);
-
 export const makeMarkupGenerator = (generator, separator = `\n`) =>
   (markups) => markups.map(generator).join(separator);
